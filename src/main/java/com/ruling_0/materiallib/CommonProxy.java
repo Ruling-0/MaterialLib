7157631fcadc2e2d76eafc31c666b9137a6d3b81
Ruling-0 <47913168+Ruling-0@users.noreply.github.com>
2026-06-13 16:36:16 -0500

    Add temporary item shape example for in-game verification

--- src/main/java/com/ruling_0/materiallib/CommonProxy.java
+++ src/main/java/com/ruling_0/materiallib/CommonProxy.java
@@ -2,6 +2,7 @@ package com.ruling_0.materiallib;
 
 import com.ruling_0.materiallib.api.ItemShapeRegistry;
 import com.ruling_0.materiallib.api.MaterialRegistry;
+import com.ruling_0.materiallib.examples.TempItemShapeExample;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -14,6 +15,9 @@ public class CommonProxy {
         Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
 
         MaterialLib.LOG.info("MaterialLib version " + Tags.VERSION);
+
+        // Temporary feature-2 verification scaffolding; removed when feature 8's examples land.
+        TempItemShapeExample.register();
     }
 
     // Mods depending on materiallib register materials in their preInit handlers, which all run before this.
